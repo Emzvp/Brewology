59cb878e62c1cdeda5490d1fdf4819a3f958b1c5
OKali <o.kalinowski27@gmail.com>
2019-07-12 12:17:20 -0400

    Changed key name for yelp key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
+//ajax call for Yelp Fusion API with work around for CORS
+
 var url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search`;
-var key =
-  "Mkvzgj8Vt_NhJJHmP7s5yNOYpOB5P6NgLK6ev6O8HyrhXvhn9nisG3WvUovYSz1PHe3cqnhK8LUThY4AoXJ1O17QoBlD-S4IlovdzhUX2BB0j5RCO3kp4w-R1WknXXYx";
+var yelpKey =
+  'Mkvzgj8Vt_NhJJHmP7s5yNOYpOB5P6NgLK6ev6O8HyrhXvhn9nisG3WvUovYSz1PHe3cqnhK8LUThY4AoXJ1O17QoBlD-S4IlovdzhUX2BB0j5RCO3kp4w-R1WknXXYx';
 
 $.ajax({
   type: "GET",
   url: url,
   headers: {
-    Authorization: "Bearer " + key
+    Authorization: "Bearer " + yelpKey
   },
   data: {
     location: "NYC", //variable for random locations
@@ -17,3 +19,8 @@ $.ajax({
   //create a variable to temporarily store a variable
   //onclick function so that response appears only when viewer clicks on the zip code or finishes the personality test
 });
+
+//ajax call for Unsplash API (photos)
+//unsplash has two keys- access and secret (not sure if both are needed)
+
+// var unsplashKey = '59666aeeb8436b49eb378f3be3581566955579bfc35d103bb0b525a48c582c13'
